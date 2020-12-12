4a26ee0b7c3603993d971c34b6c4f9b01cc710f2
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2020-12-12 18:07:41 +0500

    Added Connection for MongoDB.

--- application.js
+++ application.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const application = express();
 
 application.use(express.urlencoded({ extended: true }));
 application.use(express.json());
+
+mongoose.connect(
+  "mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 application.use(morgan("dev"));
 
